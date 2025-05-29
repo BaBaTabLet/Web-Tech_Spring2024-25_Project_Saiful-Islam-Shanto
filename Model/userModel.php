5367a0212fbb158895da2ba19b4990bc7d26d419
Shanto <shantoboss016@gmail.com>
2025-05-29 09:22:00 +0600

    userModel.php

--- Model/userModel.php
+++ Model/userModel.php
@@ -3,10 +3,10 @@
 require_once("db.php");
 function validateUser($username, $password) {
     $con =getConnection();
-    $sql ="SELECT * FROM users WHERE username = ? AND password = ?";
+    $sql ="SELECT * FROM users WHERE username= ?AND password = ?";
   
     $stmt =mysqli_prepare($con, $sql);
-    mysqli_stmt_bind_param($stmt, "ss", $username, $password) ;
+    mysqli_stmt_bind_param($stmt,"ss",$username, $password) ;
    
     mysqli_stmt_execute($stmt);
     $result=mysqli_stmt_get_result($stmt);
